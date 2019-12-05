d8c5d0ddc356b18d3ec410db83ddf41cbf683053
richyanicky <richyanicky@gmail.com>
2019-12-05 11:05:53 -0800

    added scripts for regression

---
+++ runregv2.sh
@@ -0,0 +1,10 @@
+CHROM=2
+TISSUE=Lung
+./LinRegAssociationTest_v4.py \
+  --chrom ${CHROM} \
+  --expr /storage/szfeupe/Runs/650GTEx_estr/Analysis_by_Tissue/Review_Rerun/${TISSUE}/Corr_Expr.csv \
+  --exprannot /storage/mgymrek/workspace/gtex-estrs-paper/str-regression/gencode_gene_annotations_hg19.csv \
+  --strgt /storage/ryanicky/multiallel.csv.gz \
+  --distfromgene 100000 \
+  --norm --mingt 3 \
+  --out outfile.tab
